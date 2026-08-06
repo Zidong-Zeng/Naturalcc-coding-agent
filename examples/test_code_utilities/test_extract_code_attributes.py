171ca5a247aa996e3f3432285ad624b2c2563ae8
Zidong Zeng <2652015039@qq.com>
2026-08-06 15:47:09 +0800

    Add examples/test_code_utilities/test_extract_code_attributes.py

---
+++ examples/test_code_utilities/test_extract_code_attributes.py
@@ -0,0 +1,32 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+from ncc.utils.code_util.apex.apex_code_utility import ApexCodeUtil
+from tree_sitter import Node
+
+apex_code_util = ApexCodeUtil()
+
+sample_code = """
+    public class AccountWithContacts {
+    // Method to fetch accounts and their related contacts
+    public static void getAccountsWithContacts() {
+        // Query to fetch accounts with related contacts
+        List<Account> accounts = [SELECT Id, Name, (SELECT Id, LastName, Email FROM Contacts) FROM Account LIMIT 10];
+
+        // Iterate through accounts and print account and contact information to the debug log
+        for (Account acc : accounts) {
+            System.debug('Account Name: ' + acc.Name);
+
+            for (Contact con : acc.Contacts) {
+                System.debug('Contact Name: ' + con.LastName + ', Email: ' + con.Email);
+            }
+        }
+    }
+}
+    """
+
+
+code_attributes = apex_code_util.get_code_attributes(sample_code)
+print(code_attributes)
+
+
